47c039b183d30917919a147625fb8d7f734f9214
Karel Blavka <karel.blavka@hardwario.com>
2019-12-13 22:04:45 +0100

    Add app

---
+++ app/application.h
@@ -0,0 +1,30 @@
+#ifndef _APPLICATION_H
+#define _APPLICATION_H
+
+#ifndef VERSION
+#define VERSION "vdev"
+#endif
+
+#ifndef LED_STRIP_COUNT
+#define LED_STRIP_COUNT 144
+#endif
+
+#ifndef LED_STRIP_TYPE
+#define LED_STRIP_TYPE 4
+#endif
+
+#ifndef LED_STRIP_SWAP_RG
+#define LED_STRIP_SWAP_RG 0
+#endif
+
+#include <bcl.h>
+
+typedef struct
+{
+    uint8_t channel;
+    float value;
+    bc_tick_t next_pub;
+
+} event_param_t;
+
+#endif // _APPLICATION_H
